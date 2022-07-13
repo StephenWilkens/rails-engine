e6e27e23a52ff8888c7c6b16fcdf114e49acf0e2
Stephen Wilkens <stephenwilkens@gmail.com>
2022-07-12 20:00:13 -0600

    build out merchant get and show request spec, actions, and routes

--- spec/requests/merchants_request_spec.rb
+++ spec/requests/merchants_request_spec.rb
@@ -8,5 +8,38 @@ describe 'Merchants API' do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    merchants = response_body[:data]
+
+    expect(merchants).to be_an(Array)
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(String)
+
+      expect(merchant[:type]).to be_a(String)
+
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to be_a(Hash)
+
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
+  end
+
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1//merchants/#{id}"
+
+    rb = JSON.parse(response.body, symbolize_names: true)
+    merchant = rb[:data]
+
+    expect(merchant).to have_key(:id)
+    expect(merchant).to have_key(:attributes)
+    expect(merchant[:attributes][:name]).to be_a(String)
+
   end
 end
